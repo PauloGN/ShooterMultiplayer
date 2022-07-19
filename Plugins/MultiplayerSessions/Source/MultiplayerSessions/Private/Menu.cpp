2172b69e5195b1f8128d6e02ce8178503ac90219
PauloGN <paulorsantos.gnt@outlook.com>
2022-07-19 20:32:31 -0300

    [17 - The Menu Class]
    Created a menu CPP Class to hold widget blueprints and feed the session settings
    Created a widget blueprints
    Added a host and menu Buttons

---
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Menu.h"
+
+void UMenu::MenuSetup()
+{
+	AddToViewport();
+	SetVisibility(ESlateVisibility::Visible);
+	bIsFocusable = true;
+
+	UWorld* world = GetWorld();
+
+	if (world)
+	{
+		APlayerController* playerController = world->GetFirstPlayerController();
+		if (playerController)
+		{
+			FInputModeUIOnly inputModeData;
+			inputModeData.SetWidgetToFocus(TakeWidget());
+			inputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+			playerController->SetInputMode(inputModeData);
+			playerController->SetShowMouseCursor(true);
+		}
+	}
+
+}
